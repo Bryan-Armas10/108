1d2e786b45182a0d69977cf0a0647085e6b1dabb
Bryan Omar Armas Hernandez <bryan.armas@sdgku.edu>
2024-12-06 23:14:03 -0800

    FSDI 110 assignment 1

---
+++ FSDI 108(110)/online-store/src/components/product.jsx
@@ -0,0 +1,43 @@
+import "./styles/product.css";
+import QuantityPicker from "./quantityPicker";
+import { useContext, useState } from "react";
+import GlobalContext from "../context/globalContext"
+
+function Product(props){
+    const [quantity, setQuantity] = useState(1);
+    const globalAdd =  useContext(GlobalContext).addToCart;
+
+    function handleQuantity(qty) {
+        console.log("Quantity changed", qty);
+        setQuantity(qty);
+    }
+
+    function add() {
+        let prod4Cart = { ...props.data, quantity:quantity };
+        globalAdd(prod4Cart);
+    }
+
+    function getProdTotal() {
+        let total = props.data.price * quantity;
+        return total.toFixed(2);
+    }
+
+    return(
+        <div className="product">
+            <span className="id">ID: {props.data._id}</span>
+            <img src={props.data.image} alt=""/>
+            <h3>{props.data.title}</h3>       
+
+            <div className="prices">
+                <label className="total">${getProdTotal()}</label>
+                <label className="unit">${props.data.price.toFixed(2)}</label>
+            </div>
+            <div className="parent">
+                <QuantityPicker onChange={handleQuantity}></QuantityPicker>
+                <button className="btn btn-sm btn-success" onClick={add}> Add </button>
+            </div>
+        </div>
+    );
+}
+
+export default Product;
---
+++ FSDI 108(110)/online-store/src/context/globalContext.js
@@ -0,0 +1,71 @@
+import { createContext } from "react";
+
+const GlobalContext = createContext({
+    cart: [],
+    user: {},
+
+    addToCart: () => {},
+    removeFromCart: () => {},
+    clearCart: () => {}
+});
+
+export default GlobalContext;
+
+/////////////////////////////////////////////////////////////////////////
+
+// import { createContext, useState } from "react";
+
+// // Create context
+// const GlobalContext = createContext({
+//     cart: [],
+//     catalog: [],  // New status for the product catalog
+//     user: {},
+
+//     addToCart: () => {},
+//     removeFromCart: () => {},
+//     clearCart: () => {},
+//     addProduct: () => {} // New feature to add products to the catalog
+// });
+
+// export function GlobalProvider({ children }) {
+//     const [cart, setCart] = useState([]);
+//     const [catalog, setCatalog] = useState([]); // Status for product catalog
+
+//     // Add products to cart function
+//     function addToCart(product) {
+//         setCart([...cart, product]);
+//     }
+
+//     // Function to remove products from the cart
+//     function removeFromCart(productId) {
+//         setCart(cart.filter(prod => prod._id !== productId));
+//     }
+
+//     // Function to clean the stroller
+//     function clearCart() {
+//         setCart([]);
+//     }
+
+//     // Function to add products to the catalog
+//     function addProduct(newProduct) {
+//         setCatalog([...catalog, newProduct]);
+//     }
+
+//     return (
+//         <GlobalContext.Provider
+//             value={{
+//                 cart,
+//                 catalog,
+//                 user: {},
+//                 addToCart,
+//                 removeFromCart,
+//                 clearCart,
+//                 addProduct
+//             }}
+//         >
+//             {children}
+//         </GlobalContext.Provider>
+//     );
+// }
+
+// export default GlobalContext;
